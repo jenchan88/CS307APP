43da207c2e767d37629c89d68a184498c493cd9f
jenchan88 <jechan@sonic.net>
2024-04-26 08:30:42 -0700

    get and post users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,47 +1,48 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import  userServices from "./user-service.js";
 
 const app = express();
 const port = 8000;
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
+// const users = {
+//   users_list: [
+//     {
+//       id: "xyz789",
+//       name: "Charlie",
+//       job: "Janitor"
+//     },
+//     {
+//       id: "abc123",
+//       name: "Mac",
+//       job: "Bouncer"
+//     },
+//     {
+//       id: "ppp222",
+//       name: "Mac",
+//       job: "Professor"
+//     },
+//     {
+//       id: "yat999",
+//       name: "Dee",
+//       job: "Aspring actress"
+//     },
+//     {
+//       id: "zap555",
+//       name: "Dennis",
+//       job: "Bartender"
+//     }
+//   ]
+// };
 
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
+// const findUserByName = (name) => {
+//   return users["users_list"].filter(
+//     (user) => user["name"] === name
+//   );
+// };
 
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
+// const findUserById = (id) =>
+//   users["users_list"].find((user) => user["id"] === id);
 
 // const addUser = (user) => {
 //   const newUser = { ...user, id: createRandomId() };
@@ -49,47 +50,47 @@ const findUserById = (id) =>
 //   return user;
 // };
 
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
+// const addUser = (user) => {
+//   users["users_list"].push(user);
+//   return user;
+// };
 
 
-const findUserByJob = (job) => {
-  return users["users_list"].filter(
-    (user) => user["job"] === job
-  );
-};
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
+// const findUserByJob = (job) => {
+//   return users["users_list"].filter(
+//     (user) => user["job"] === job
+//   );
+// };
+// const findUserByNameAndJob = (name, job) => {
+//   return users["users_list"].filter(
+//     (user) => user["name"] === name && user["job"] === job
+//   );
+// };
 
 
-const createRandomID = () => {
-  const chars = 'abcdefghiklmnopqrstuvwxyz0123456789'.split('');
-  let str = '';
+// const createRandomID = () => {
+//   const chars = 'abcdefghiklmnopqrstuvwxyz0123456789'.split('');
+//   let str = '';
   
-  for (let i = 0; i < 3; i++) {
-    str += chars[Math.floor(Math.random() * chars.length)];
-  }
+//   for (let i = 0; i < 3; i++) {
+//     str += chars[Math.floor(Math.random() * chars.length)];
+//   }
 
-  for (let i = 0; i < 3; i++) {
-    str += chars[Math.floor(Math.random() * chars.length)];
-  }
+//   for (let i = 0; i < 3; i++) {
+//     str += chars[Math.floor(Math.random() * chars.length)];
+//   }
 
-  return str;
-}
+//   return str;
+// }
 
 app.use(cors());
 
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port, () => {
   console.log(
@@ -109,71 +110,7 @@ app.listen(port, () => {
 //   }
 // });
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  const job = req.query.job;
-
-  if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } 
-
-  else if (name != undefined && job === undefined){
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  }
-
-  else if (name === undefined && job != undefined){
-    let result = findUserByJob(job);
-    result = { users_list: result };
-    res.send(result);
-  }
-
-  else {
-    res.send(users.users_list)
-  }
-});
-
-
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
-});
-
-
-
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  if(userToAdd["id"] === undefined){
-    userToAdd["id"] = createRandomID()
-  }
-  addUser(userToAdd);   
-  res.status(201).send(userToAdd);
-});
-
-
-app.delete("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const index = users.users_list.findIndex(user => user.id === id)
-  console.log(id)
-  if (index != -1){
-    users.users_list.splice(index, 1);
-    res.status(204).send();
-  }
-  else{
-    res.status(404).send("User not found.");
-  }
-});
-
-// app.get("/users-by-job", (req, res) => {
+// app.get("/users", (req, res) => {
 //   const name = req.query.name;
 //   const job = req.query.job;
 
@@ -182,9 +119,130 @@ app.delete("/users/:id", (req, res) => {
 //     result = { users_list: result };
 //     res.send(result);
 //   } 
-//   else {
-//     res.status(400).send("name and job params required");
+
+//   else if (name != undefined && job === undefined){
+//     let result = findUserByName(name);
+//     result = { users_list: result };
+//     res.send(result);
 //   }
 
+//   else if (name === undefined && job != undefined){
+//     let result = findUserByJob(job);
+//     result = { users_list: result };
+//     res.send(result);
+//   }
+
+//   else {
+//     res.send(users.users_list)
+//   }
 // });
+
+
+
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params["id"]; //or req.params.id
+//   let result = findUserById(id);
+//   if (result === undefined) {
+//     res.status(404).send("Resource not found.");
+//   } else {
+//     res.send(result);
+//   }
+// });
+
+
+
+// app.post("/users", (req, res) => {
+//   const userToAdd = req.body;
+//   if(userToAdd["id"] === undefined){
+//     userToAdd["id"] = createRandomID()
+//   }
+//   addUser(userToAdd);   
+//   res.status(201).send(userToAdd);
+// });
+
+
+// app.delete("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const index = users.users_list.findIndex(user => user.id === id)
+//   console.log(id)
+//   if (index != -1){
+//     users.users_list.splice(index, 1);
+//     res.status(204).send();
+//   }
+//   else{
+//     res.status(404).send("User not found.");
+//   }
+// });
+
+//new stuff
+
+
+
+
+// app.post("/users", async (req, res) => {
+//   const userToAdd = req.body;
+//   userService
+//   .addUser(userToAdd)
+//   .then((result) => res.status(201).send(result));
+// });
+
+app.post("/users", async (req, res) => {
+  const userToAdd = req.body;
+  userServices.addUser(userToAdd)
+    .then((result) => res.status(201).send(result))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+// app.get("/users/:id", async (req, res) => {
+//   const id = req.params["id"];
+//   userServices.findUserById(id)
+//   .then((result) => {
+//   if (result) {
+//   res.send(result);
+//   } 
+//   else {
+//   res.status(404).send(`Not Found: ${id}`);
+//   }
+//   })
+//   .catch((error) => {
+//   res.status(500).send(error.name);
+// })
+
+// });
+
+
+app.get("/users", (req, res) => {
+  const { name, job } = req.query;
+  userServices.getUsers(name, job)
+  .then((result) => {
+  if (result){
+    res.send({ users_list: result });
+  }
+  else{
+    res.status(404).send("Not Found");
+  }
+  })
+  .catch ((error) =>{ 
+    console.log(error)
+    res.status(500).send(error.message);
+  })
+});
+
+// app.delete("/users/:id", async (req, res) => {
+//   const id = req.params.id;
+//   await userService.deleteUser(id)
+//   .then((result) => {
+//   if (result.deletedCount === 0) {
+//     res.status(404).send("User not found.");
+//   } else {
+//     res.status(204).send();
+//   }
+//   }) 
+//   .catch ((error) => {
+//     res.status(500).send(error.message);
+//   })
+// });
+
+
+
 
